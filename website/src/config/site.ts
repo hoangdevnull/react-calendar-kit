523b691a1a30170eab54dec50bf30a7c195d82ae
HoangPham99 <pqhoang.dev@gmail.com>
2024-05-20 15:11:24 +0700

    feat: shorten description

--- website/src/config/site.ts
+++ website/src/config/site.ts
@@ -5,9 +5,9 @@ export const APP_URL = 'https://react-calendar-kit.vercel.app';
 export const siteConfig = {
   name: 'React-Calendar-kit - Documentation',
   metaTitle:
-    'React Calendar Kit is a powerful and flexible library for building accessible and highly customizable calendar and date/time picker components in your React applications. Built on top of the robust accessibility features of the @react-aria library, React Calendar Kit provides a solid foundation for creating inclusive user experiences. | react-calendar-kit.vercel.app',
+    'React Calendar Kit is a powerful and flexible library for building accessible and highly customizable calendar and date/time picker components in your React applications. | react-calendar-kit.vercel.app',
   description:
-    'React Calendar Kit is a powerful and flexible library for building accessible and highly customizable calendar and date/time picker components in your React applications. Built on top of the robust accessibility features of the @react-aria library, React Calendar Kit provides a solid foundation for creating inclusive user experiences. | react-calendar-kit.vercel.app',
+    'React Calendar Kit is a powerful and flexible library for building accessible and highly customizable calendar and date/time picker components in your React applications. | react-calendar-kit.vercel.app',
   ogImage: `${APP_URL}/og-image.jpg`,
   github: 'https://github.com/hoangdevnull/react-calendar-kit',
 };
